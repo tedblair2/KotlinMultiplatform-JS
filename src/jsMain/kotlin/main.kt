f9866903dff8fabf4a68112f9a0a700bedeb4f6c
Ted Blair Omino <ominoblair@gmail.com>
2024-05-12 17:38:15 +0300

    Cleanup

--- src/jsMain/kotlin/main.kt
+++ src/jsMain/kotlin/main.kt
@@ -5,7 +5,7 @@ import model.users
 external fun require(module:String):dynamic
 
 fun main() {
-    println("Hello Kotlin/JS Another")
+    println("Hello Kotlin/JS")
     val express=require("express")
     val app=express()
     app.listen(3000) {
